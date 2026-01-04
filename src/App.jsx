b8b0856b2971bbb174c50398bd3583192d699ca0
danielcanunciato <danielcanunciatooliveira@gmail.com>
2026-01-04 13:43:15 -0300

    04-01-25

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [currentProgress, setCurrentProgress] = useState(65)
+
+  return (
+    <>
+      <h1>Hi!</h1>
+      <p>This is a placeholder website because the new one is still being made.<br />Sorry for it taking so long, I was taking a break from coding.</p>
+
+      <h1 style={{color: `gold`, marginTop: `90px`}}>!!!PROGRESS BAR!!!</h1>
+      <p style={{transform: `translateY(-40px)`}}>Below will be a funny progress bar for the next games in the meantime.</p>
+
+      <h2 style={{color: `gold`}}>Lights Out: Relight <br /><span style={{color: `white`, fontSize: `3rem`, lineHeight: `1`}}>{currentProgress}%</span></h2>
+      <progress style={{transform:`translateY(-20px)`, accentColor: `red`}} value={currentProgress} max="100"></progress>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 20122, // 20th of January of 2022 || Super Platform Birthday
+  },
+})
