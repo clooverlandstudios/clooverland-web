c8f0de05e993c7a7ecf083340b35ad4691d525b3
Clooverland Studios <clooverlandstudios@gmail.com>
2026-01-29 23:44:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
 
-  const [currentProgress, setCurrentProgress] = useState(92)
+  const [currentProgress, setCurrentProgress] = useState(95)
 
   return (
     <>
